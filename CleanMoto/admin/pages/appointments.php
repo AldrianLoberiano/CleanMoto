863f6a113b4f081e00ba938615815dc938ef520f
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:38:44 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -81,3 +81,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
             LEFT JOIN users u ON a.user_id = u.id
             $whereClause
             ORDER BY a.appointment_date DESC, a.appointment_time DESC
+        ";
