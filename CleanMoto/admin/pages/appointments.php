71c2e32db37b57a82d13f5598ccabf054dfcf1d7
AldrianLoberiano <loberianorian@gmail.com>
2026-04-10 08:37:20 +0800

    Update appointments.php

--- CleanMoto/admin/pages/appointments.php
+++ CleanMoto/admin/pages/appointments.php
@@ -48,3 +48,4 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     // Status filter
     if ($statusFilter !== 'all') {
         $whereConditions[] = "a.status = :status";
+        $params[':status'] = $statusFilter;
